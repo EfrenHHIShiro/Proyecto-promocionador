c8847b2b699ba66e3200718ac2549d1c4366014a
Bryan <bryanpech@hotmail.com>
2020-11-03 14:46:34 -0600

    Mapsjalando-easy
    donde le subo la dificultad?

--- Frontend/website/src/components/GoogleMap.js
+++ Frontend/website/src/components/GoogleMap.js
@@ -1,42 +1,90 @@
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import React,{Component} from 'react'
+import {useLocation} from 'react-router-dom'
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+  } from 'react-places-autocomplete';
+  import Grid from '@material-ui/core/Grid';
 
 const containerStyle = {
     width: '25%',
     height: '50%'
   }
 export class MapContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { address: '',
 
-    state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
-
-      mapCenter: {
-          lat: 20.9753700,
-          lng:  -89.6169600,
-      }
-    };
-    
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+  
+        mapCenter: {
+            lat: 20.9753700,
+            lng:  -89.6169600,
+        } 
+    }
+ };
    
-    onMarkerClick = (props, marker, e) =>
-      this.setState({
-        selectedPlace: props,
-        activeMarker: marker,
-        showingInfoWindow: true
-      });
-   
-    onMapClicked = (props) => {
-      if (this.state.showingInfoWindow) {
-        this.setState({
-          showingInfoWindow: false,
-          activeMarker: null
-        })
-      }
-    };
+    handleChange = address => {
+        this.setState({ address });
+      };
+     
+      handleSelect = address => {
+        geocodeByAddress(address)
+          .then(results => getLatLng(results[0]))
+          .then(latLng =>{
+            console.log('Success', latLng.lat)
+            this.setState({address})
+            this.setState({mapCenter: latLng})
+          })
+          .catch(error => console.error('Error', error));
+      };
    
     render() {
       return (
+          <div>
+                   <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
         <Map google={this.props.google} 
             zoom={14 }
             containerStyle={containerStyle}
@@ -55,6 +103,9 @@ export class MapContainer extends Component {
             lng: this.state.mapCenter.lng
           }}/>
         </Map>
+        </Grid>
+        </Grid>
+        </div>
       )
     }
 }
@@ -62,5 +113,5 @@ export class MapContainer extends Component {
   
 
   export default GoogleApiWrapper({
-    apiKey: ('AIzaSyCbLvp4R-WoJOlAQo_UPT6jE5VXnz94yTw')
+    apiKey: ('AIzaSyB-lgHBNL41XX6C8k6IPAjMOAA4anR3QGo')
   })(MapContainer)
